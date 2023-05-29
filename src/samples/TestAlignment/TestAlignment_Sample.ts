3b5ec6f208b479e2bbe759759e0f9adbe317abc9
tlecoz <fanthomas.lecoz@gmail.com>
2023-05-30 01:08:10 +0200

    add some tests in TestAlignment

--- src/samples/TestAlignment/TestAlignment_Sample.ts
+++ src/samples/TestAlignment/TestAlignment_Sample.ts
@@ -1,4 +1,4 @@
-import { BuiltIns, Float, GPURenderer, Matrix2x2, Matrix2x3, Matrix3x3, Matrix4x4, RenderPipeline, TextureSampler, UniformGroup, UniformGroupArray, Vec2, Vec3, Vec4, Vec4Array } from "xgpu";
+import { BuiltIns, Float, GPURenderer, IVec4, IVec4Array, Matrix2x2, Matrix2x3, Matrix3x3, Matrix4x4, RenderPipeline, TextureSampler, UniformGroup, UniformGroupArray, Vec2, Vec3, Vec4, Vec4Array } from "xgpu";
 import { Sample } from "../HelloTriangle/Sample";
 import { vec3 } from "gl-matrix";
 import { ImageTextureArray } from "xgpu/src/xGPU/shader/resources/ImageTextureArray";
@@ -45,9 +45,57 @@ export class TestAlignment_Sample extends Sample {
 
     protected async start(renderer: GPURenderer): Promise<void> {
 
+        this.test03(renderer);
 
 
-        /*
+    }
+
+    private test03(renderer: GPURenderer) {
+
+
+
+        const pipeline = new RenderPipeline(renderer);
+        pipeline.initFromObject({
+
+            col: new IVec4Array([
+                new IVec4(100, 0, 0, 255),
+                new IVec4(100, 255, 0, 255),
+                new IVec4(255, 127, 255, 255),
+            ]),
+            //tab:new Vec2Array()
+            vertexId: BuiltIns.vertexInputs.vertexIndex,
+            vertexCount: 6,
+            uv: BuiltIns.vertexOutputs.Vec2,
+            textureId: BuiltIns.vertexOutputs.Float,
+            vertexShader: {
+                constants: `
+               const pos = array<vec2<f32>,6>(
+                   vec2(-1.0, -1.0),
+                   vec2(1.0, -1.0),
+                   vec2(-1.0, 1.0),
+                   vec2(1.0, -1.0),
+                   vec2(1.0, 1.0),
+                   vec2(-1.0, 1.0),
+                );
+               `,
+                main: `
+               output.position = vec4(pos[vertexId],0.0,1.0);
+               output.uv = (pos[vertexId] + 1.0) * 0.5;
+               output.textureId = 0;
+               `
+            },
+            fragmentShader: `
+               output.color = vec4<f32>(col[2])/255.0;
+           `
+        })
+
+        renderer.addPipeline(pipeline);
+
+    }
+
+
+    private test02(renderer: GPURenderer) {
+
         const classes = [Float, Vec2, Vec3, Vec4, Matrix4x4, Matrix2x2, Matrix2x3, Matrix3x3];
         const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
@@ -56,19 +104,63 @@ export class TestAlignment_Sample extends Sample {
         }
 
         const o = {};
+        //let nb = 8000;
+        let nb = 100;
         let test = "";
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < nb; i++) {
             const n = Math.floor((Math.random() * 100) / (100 / classes.length))
             const name = getChar() + getChar() + getChar() + "_" + Math.floor(Math.random() * 1000);
             o[name] = new classes[n]();
-            if (i === 50) test = name;
+            if (i === nb - 10) test = name;
         }
 
         o[test] = new Vec3(1.0, 0.0, 1.0);
 
         var myGroup = new UniformGroup(o);
-        */
 
+
+        const pipeline = new RenderPipeline(renderer);
+        pipeline.initFromObject({
+
+            myGroup,
+
+            a: new Float(0.25),
+            mouse: new Mouse(renderer.canvas),
+            b: new Vec2(0.5, 0.0),
+            c: new Float(0.75),
+            //tab:new Vec2Array()
+            vertexId: BuiltIns.vertexInputs.vertexIndex,
+            vertexCount: 6,
+            uv: BuiltIns.vertexOutputs.Vec2,
+            textureId: BuiltIns.vertexOutputs.Float,
+            vertexShader: {
+                constants: `
+               const pos = array<vec2<f32>,6>(
+                   vec2(-1.0, -1.0),
+                   vec2(1.0, -1.0),
+                   vec2(-1.0, 1.0),
+                   vec2(1.0, -1.0),
+                   vec2(1.0, 1.0),
+                   vec2(-1.0, 1.0),
+                );
+               `,
+                main: `
+               output.position = vec4(pos[vertexId],0.0,1.0);
+               output.uv = (pos[vertexId] + 1.0) * 0.5;
+               output.textureId = 0;
+               `
+            },
+            fragmentShader: `
+               output.color = vec4(myGroup.${test},1.0);
+           `
+        })
+
+        renderer.addPipeline(pipeline);
+
+    }
+
+
+    private test01(renderer: GPURenderer) {
 
 
         var otherObj = {
@@ -79,7 +171,7 @@ export class TestAlignment_Sample extends Sample {
         }
 
 
-        const test = "ee.ccc";
+        let test = "ee.ccc";
         var myGroup = new UniformGroup({
             aa: new Float(1),
             bb: new Vec2(1.0, 1.0),
@@ -91,7 +183,7 @@ export class TestAlignment_Sample extends Sample {
 
         const groupArray = new UniformGroupArray([
             myGroup,
-            new UniformGroup({ aaaaaaaaaaa: new Float(123) })
+            myGroup,
         ])
 
         const images = new ImageTextureArray({
@@ -103,9 +195,12 @@ export class TestAlignment_Sample extends Sample {
         pipeline.initFromObject({
             //obj,
             //myGroup,
+
             imgSampler: new TextureSampler(),
             images,
             groupArray,
+            //myGroup,
+
             a: new Float(0.25),
             mouse: new Mouse(renderer.canvas),
             b: new Vec2(0.5, 0.0),
@@ -129,21 +224,20 @@ export class TestAlignment_Sample extends Sample {
                 main: `
                 output.position = vec4(pos[vertexId],0.0,1.0);
                 output.uv = (pos[vertexId] + 1.0) * 0.5;
-                output.textureId = 1;
+                output.textureId = 0;
                 `
             },
             fragmentShader: `
                 //output.color = vec4(a,a,a,1.0);
                 //output.color = vec4(mouse.x,mouse.y,mouse.x*mouse.y,1.0);
+                
+                //output.color = vec4(myGroup.${test},1.0);
+                
                 //output.color = vec4(groupArray[0].${test},1.0);
-                output.color = textureSample(images,imgSampler,uv,i32(textureId));
+                //output.color = textureSample(images,imgSampler,uv,i32(textureId));
             `
         })
 
         renderer.addPipeline(pipeline);
-
-
     }
-
-
 }
