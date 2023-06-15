7de20a7a9f5e42e6abff4d5cc98158713ef9479b
tlecoz <fanthomas.lecoz@gmail.com>
2023-06-15 06:28:38 +0200

    vite has been updated ; cleaned renderBundle_sample

--- src/samples/RenderBundle/RenderBundle_Sample.ts
+++ src/samples/RenderBundle/RenderBundle_Sample.ts
@@ -1,4 +1,4 @@
-import { Bindgroup, BuiltIns, GPURenderer, ImageTexture, IndexBuffer, RenderPipeline, TextureSampler, UniformBuffer, VertexAttribute, VertexBuffer } from "xgpu";
+import { Bindgroup, BuiltIns, GPURenderer, ImageTexture, IndexBuffer, TextureSampler, UniformBuffer, VertexAttribute, VertexBuffer } from "xgpu";
 import { Sample } from "../HelloTriangle/Sample";
 import { createSphereMesh, SphereLayout } from "../../meshes/Sphere";
 import { ResizableRenderPipeline } from "../HelloTriangle/ResizableRenderPipeline";
@@ -10,30 +10,7 @@ export class RenderBundle_Sample extends Sample {
 
     public async start(renderer: GPURenderer): Promise<void> {
 
-
-
-
-        const createPolygon = async (posx: number, nbSide: number) => {
-            if (nbSide < 3) nbSide = 3;
-
-            const pos = [];
-            let angle;
-            let addAngle = (Math.PI * 2) / nbSide;
-            let indices = [];
-            let n = 0;
-
-
-            for (let i = 0; i < nbSide; i++) {
-                angle = -Math.PI / 2 + addAngle * i;
-                pos.push(Math.cos(angle), Math.sin(angle));
-
-                indices.push(nbSide, (n + 0) % (nbSide), (n + 1) % (nbSide))
-                n++;
-
-            }
-            pos.push(0, 0);
-            //console.log(nbSide + " :::: ", indices);
-
+        const createCanvas = (val) => {
             const canvas = document.createElement("canvas");
             const ctx = canvas.getContext("2d");
             canvas.width = canvas.height = 256;
@@ -41,46 +18,36 @@ export class RenderBundle_Sample extends Sample {
             ctx.fillRect(0, 0, 256, 256);
             ctx.fillStyle = "#ffffff";
             ctx.font = "100px Verdana";
-            ctx.fillText("" + nbSide, 100, 160)
-            const bmp = await createImageBitmap(canvas)
+            ctx.fillText("" + val, 100, 160)
+            return canvas
+        }
 
 
+        const createPolygon = async (posx: number, nbSide: number) => {
+
+            const pos = [];
+            let indices = [];
+
+            for (let i = 0; i < nbSide; i++) {
+                let angle = ((Math.PI * 2) / nbSide) * i;
+                pos.push(Math.cos(angle), Math.sin(angle));
+                indices.push(nbSide, (i + 0) % (nbSide), (i + 1) % (nbSide))
+            }
+
+            pos.push(0, 0);
 
             const matrix = new ModelViewMatrix();
             matrix.scaleX = matrix.scaleY = 100;
             matrix.x = posx
 
-            const desc = { source: bmp, debug: "AZERTY_" + nbSide } as any
-            console.log("desc.debug = ", desc.debug);
-
-            const img: any = new ImageTexture(desc);
-            img.debug = "nbSide=" + nbSide;
-
-            const indexBuffer = new IndexBuffer({ nbPoint: indices.length });
-            indexBuffer.datas = new Uint32Array(indices);
             return {
-                vertexBuffer: new VertexBuffer({
-                    position: VertexAttribute.Vec2(pos)
-                }),
-                properties: new UniformBuffer({
-                    matrix,
-
-                }),
-                indexBuffer,
-                img,
+                vertexBuffer: new VertexBuffer({ position: VertexAttribute.Vec2(pos) }),
+                properties: new UniformBuffer({ matrix }),
+                indexBuffer: new IndexBuffer({ nbPoint: indices.length, datas: new Uint32Array(indices) }),
+                img: new ImageTexture({ source: createCanvas(nbSide) }),
                 imgSampler: new TextureSampler(),
             };
         }
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -101,15 +68,11 @@ export class RenderBundle_Sample extends Sample {
         }
 
 
-
-
-
         const pipeline = new ResizableRenderPipeline(renderer);
         pipeline.initFromObject({
             bindgroups: {
                 poly: bindgroup
             },
-
 
             camera: new Camera(renderer.width, renderer.height, 45),
             indexBuffer: (poly as any).indexBuffer,
@@ -123,46 +86,9 @@ export class RenderBundle_Sample extends Sample {
             `
         })
 
-        /*
-        let bool = true;
 
-        pipeline.onDrawEnd = () => {
-            if (bool) {
-                bool = false;
-
-                polygons[2]._object.apply();
-            }
-        }
-        */
 
         renderer.addPipeline(pipeline);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
-        const buffer = new VertexBuffer({
-            position:VertexAttribute.Vec3(SphereLayout.positionsOffset),
-            normal:VertexAttribute.Vec3(SphereLayout.normalOffset),
-            uv:VertexAttribute.Vec2(SphereLayout.uvOffset)
-        });
-        buffer.setComplexDatas(sphereMesh.vertice,SphereLayout.vertexStride)
-
-
-        pipeline.initFromObject({
-            
-        });
-        */
 
 
     }
